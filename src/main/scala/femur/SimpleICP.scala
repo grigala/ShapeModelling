7f001ae347cde20122380fc33b61374033c5c58a
fabricio.arendtorres@stud.unibas.ch <fabricio.arendtorres@stud.unibas.ch>
2017-04-14 03:41:25 +0200

    refactored code and created a function for getting the transformations, given the reference ShapeModel and the target Shape

--- src/main/scala/femur/SimpleICP.scala
+++ src/main/scala/femur/SimpleICP.scala
@@ -5,11 +5,9 @@ import java.io.File
 import scalismo.common._
 import scalismo.geometry._
 import scalismo.io._
-import scalismo.kernels._
-import scalismo.numerics._
 import scalismo.statisticalmodel._
 import scalismo.ui.api.SimpleAPI.ScalismoUI
-import scalismo.mesh.{Mesh, TriangleMesh}
+
 object SimpleICP {
 
   def main(args: Array[String]) {
@@ -19,7 +17,7 @@ object SimpleICP {
 
     ////////////////////SETTINGS FOR ICP
     val numIterations = 15
-    val noise = NDimensionalNormalDistribution(Vector(0,0,0), SquareMatrix((1f,0,0), (0,1f,0), (0,0,1f)))
+    val noise = NDimensionalNormalDistribution(Vector(0, 0, 0), SquareMatrix((1f, 0, 0), (0, 1f, 0), (0, 0, 1f)))
 
     // create a visualization window
     val ui = ScalismoUI()
@@ -36,48 +34,15 @@ object SimpleICP {
     ui.show(model, "model")
 
     val target = MeshIO.readMesh(new File("data/aligned/meshes/femur_0.stl")).get
-    //val targetSampler = RandomMeshSampler3D(target, 500, 42)
-    ui.show(target,"target")
+    //ui.show(target,"target")
 
-    val pointSamples = UniformMeshSampler3D(model.mean, 12000, 42).sample.map(s => s._1)
-    val pointIds = pointSamples.map{s => model.mean.findClosestPoint(s).id}
-    //println(pointIds.take(10))
+    //model: StatisticalMeshModel, target: TriangleMesh, noise:
+    //   NDimensionalNormalDistribution[_3D], numIterations: Int)
+    val transformationField: DiscreteVectorField[_3D, _3D] =
+    calcCorrespondentTransformationsICP(model, target, noise, numIterations)
 
-    def attributeCorrespondences(pts : Seq[Point[_3D]]) : Seq[Point[_3D]] = {
-      pts.map{pt => target.findClosestPoint(pt).point}
-    }
 
-    def fitModel(pointIds: IndexedSeq[PointId],candidateCorresp: Seq[Point[_3D]]) :TriangleMesh = {
-      val trainingData = (pointIds zip candidateCorresp).map{ case (mId, pPt) =>
-        (mId, pPt, noise)
-      }
-      val posterior = model.posterior(trainingData.toIndexedSeq)
-      posterior.mean
-    }
-
-    def recursion(currentPoints : Seq[Point[_3D]], nbIterations : Int) : Seq[Point[_3D]]= {
-
-      val candidates = attributeCorrespondences(currentPoints)
-      val fit = fitModel(pointIds, candidates)
-      val newPoints = pointIds.map(id => fit.point(id))
-      if(nbIterations> 0) {
-        //Thread.sleep(3000)
-        recursion(newPoints, nbIterations - 1)
-      }else{
-        ui.remove("fit")
-        ui.show(fit,"fit")
-        newPoints
-      }
-    }
-
-    val newPoints = recursion( pointIds.map(id => model.mean.point(id)) , numIterations)
-    val targetPoints = attributeCorrespondences(newPoints)
-    val refPoints = pointIds.map(id => model.mean.point(id))
-
-    val domain = UnstructuredPointsDomain[_3D](refPoints.toIndexedSeq)
-    val values =  (refPoints.zip(targetPoints)).map{case (mPt, pPt) => pPt - mPt}
-    val field = DiscreteVectorField(domain, values.toIndexedSeq)
-    ui.show(field, "deformations")
+    ui.show(transformationField, "deformations")
 
 
     //println(pointIds.take(10))
@@ -87,8 +52,8 @@ object SimpleICP {
 
     //val refPoints = targetIDs.slice(5,6).map{id => model.mean.point(id)}
     //val targetPoints = targetIDs.slice(5,6).map{id => target.point(id)}
-    ui.show(refPoints, "refPoints")
-    ui.show(targetPoints, "targetPoints")
+    //ui.show(refPoints, "refPoints")
+    //ui.show(targetPoints, "targetPoints")
     print("done")
   }
 }
---
+++ src/main/scala/femur/package.scala
@@ -0,0 +1,57 @@
+import scalismo.common.{DiscreteVectorField, PointId, UnstructuredPointsDomain}
+import scalismo.geometry.{Point, _3D}
+import scalismo.mesh.TriangleMesh
+import scalismo.numerics.UniformMeshSampler3D
+import scalismo.statisticalmodel._
+
+/**
+  * Created by Fabricio on 14.04.2017.
+  */
+package object femur {
+
+  def calcCorrespondentTransformationsICP(model: StatisticalMeshModel, target: TriangleMesh, noise:
+  NDimensionalNormalDistribution[_3D], numIterations: Int): DiscreteVectorField[_3D, _3D] = {
+
+    val pointSamples = UniformMeshSampler3D(model.mean, 12000, 42).sample.map(s => s._1)
+    val pointIds = pointSamples.map { s => model.mean.findClosestPoint(s).id }
+
+
+    def attributeCorrespondences(pts: Seq[Point[_3D]]): Seq[Point[_3D]] = {
+      pts.map { pt => target.findClosestPoint(pt).point }
+    }
+
+    def fitModel(pointIds: IndexedSeq[PointId], candidateCorresp: Seq[Point[_3D]]): TriangleMesh = {
+      val trainingData = (pointIds zip candidateCorresp).map { case (mId, pPt) =>
+        (mId, pPt, noise)
+      }
+      val posterior = model.posterior(trainingData.toIndexedSeq)
+      posterior.mean
+    }
+
+    def recursion(currentPoints: Seq[Point[_3D]], nbIterations: Int): Seq[Point[_3D]] = {
+
+      val candidates = attributeCorrespondences(currentPoints)
+      val fit = fitModel(pointIds, candidates)
+      val newPoints = pointIds.map(id => fit.point(id))
+      if (nbIterations > 0) {
+        //Thread.sleep(3000)
+        recursion(newPoints, nbIterations - 1)
+      } else {
+        //ui.remove("fit")
+        //ui.show(fit,"fit")
+        newPoints
+      }
+    }
+
+
+    val fittedModelPoints = recursion(pointIds.map(id => model.mean.point(id)), numIterations)
+    val targetPoints = attributeCorrespondences(fittedModelPoints)
+    val refPoints = pointIds.map(id => model.mean.point(id))
+
+    val domain = UnstructuredPointsDomain[_3D](refPoints.toIndexedSeq)
+    val diff = (refPoints.zip(targetPoints)).map { case (mPt, pPt) => pPt - mPt }
+
+    DiscreteVectorField(domain, diff.toIndexedSeq)
+  }
+
+}
