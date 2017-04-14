d2650f7cde278c87efda8d5344c7b14b8fe135ad
fabricio.arendtorres@stud.unibas.ch <fabricio.arendtorres@stud.unibas.ch>
2017-04-14 07:20:40 +0200

    shape model from data

--- src/main/scala/femur/SimpleICP.scala
+++ src/main/scala/femur/SimpleICP.scala
@@ -5,6 +5,8 @@ import java.io.File
 import scalismo.common._
 import scalismo.geometry._
 import scalismo.io._
+import scalismo.kernels.{DiagonalKernel, GaussianKernel}
+import scalismo.numerics.RandomMeshSampler3D
 import scalismo.statisticalmodel._
 import scalismo.ui.api.SimpleAPI.ScalismoUI
 
@@ -13,7 +15,7 @@ object SimpleICP {
   def main(args: Array[String]) {
 
     ////////////////////SETTINGS FOR ICP
-    val numIterations = 15
+    val numIterations = 20
     val noise = NDimensionalNormalDistribution(Vector(0, 0, 0), SquareMatrix((1f, 0, 0), (0, 1f, 0), (0, 0, 1f)))
 
     // required to initialize native libraries (VTK, HDF5 ..)
@@ -36,7 +38,7 @@ object SimpleICP {
     //val target = MeshIO.readMesh(new File("data/aligned/meshes/femur_0.stl")).get
     //val transformationField: DiscreteVectorField[_3D, _3D] =
     //  calcCorrespondentTransformationsICP(model, target, noise, numIterations)
-    println("Calculate transformation fields from data...")
+    println("Calculate transformation fields from data with ICP...")
     val dataset = files.map { f: File => MeshIO.readMesh(f).get }
     val transformationFields : IndexedSeq[DiscreteVectorField[_3D, _3D]] = dataset.map{ targetMesh =>
       calcCorrespondentTransformationsICP(model, targetMesh, noise, numIterations)
@@ -44,11 +46,26 @@ object SimpleICP {
     println("Calculate Statistical Mesh Model from data...")
     val continuousFields = transformationFields.map(f => f.interpolateNearestNeighbor)
     val dataGP = DiscreteLowRankGaussianProcess.createUsingPCA(refFemur, continuousFields)
-    val finalModel = StatisticalMeshModel(refFemur, dataGP.interpolateNearestNeighbor)
-    ui.show(finalModel, "dataShapeModel")
+    val dataModel = StatisticalMeshModel(refFemur, dataGP.interpolateNearestNeighbor)
+    ui.show(dataModel, "dataShapeModel")
 
+    StatismoIO.writeStatismoMeshModel(dataModel, new File("data/data_shape_model.h5"))
+
+
+    /*
+    println("augment kernel..")
+    //val customKernel = gp.cov
+    val augmentedKernel = dataGP.interpolateNearestNeighbor.cov + model.gp.interpolateNearestNeighbor.cov
+    val zeroMean = VectorField(RealSpace[_3D], (pt:Point[_3D]) => Vector(0,0,0))
+    val sampler = RandomMeshSampler3D(refFemur, 300, 42)
+    println("Do lowrank approximation...")
+    val gp = GaussianProcess(zeroMean, augmentedKernel)
+    val lowRankGP : LowRankGaussianProcess[_3D, _3D] = LowRankGaussianProcess.approximateGP(gp, sampler, 30)
+    val finalModel = StatisticalMeshModel(refFemur, lowRankGP)
+
+    ui.show(finalModel, "augmented model")
     //ui.show(transformationField, "deformations")
-
+    */
     print("done")
   }
 }
