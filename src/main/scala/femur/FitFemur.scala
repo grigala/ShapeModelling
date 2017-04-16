cc103b2bedb31067fefb96bc4995be3f6207fe94
fabricio.arendtorres@stud.unibas.ch <fabricio.arendtorres@stud.unibas.ch>
2017-04-16 23:22:14 +0200

    removed code from comments, added variable for selecting femur to reconstruct

--- src/main/scala/femur/FitFemur.scala
+++ src/main/scala/femur/FitFemur.scala
@@ -2,7 +2,6 @@ package femur
 
 import java.io.File
 
-import breeze.linalg.norm
 import scalismo.common.{DiscreteVectorField, PointId, UnstructuredPointsDomain}
 import scalismo.geometry._
 import scalismo.io._
@@ -16,70 +15,59 @@ object FitFemur {
 
   def main(args: Array[String]) {
 
+    val reconstructFemurNo = 1
+
+
     scalismo.initialize()
 
     ////////////////////SETTINGS FOR ICP
     val numIterations = 50
     val noise = NDimensionalNormalDistribution(Vector(0, 0, 0), SquareMatrix((1f, 0, 0), (0, 1f, 0), (0, 0, 1f)))
+
     //
     val ui = ScalismoUI()
-//
+    //
     println("Loading and displaying partial mesh...")
-    val target: TriangleMesh = MeshIO.readMesh(new File("data/partials/VSD.Right_femur.XX.XX.OT.101149.0.stl")).get
+    val targetName: String = reconstructFemurNo match {
+      case 1 => "data/partials/VSD.Right_femur.XX.XX.OT.101147.0.stl"
+      case 2 => "data/partials/VSD.Right_femur.XX.XX.OT.101148.0.stl"
+      case 3 => "data/partials/VSD.Right_femur.XX.XX.OT.101149.0.stl"
+      case 4 => "data/partials/VSD.Right_femur.XX.XX.OT.101150.0.stl"
+      case 5 => "data/partials/VSD.Right_femur.XX.XX.OT.101151.0.stl"
+      case 6 => "data/partials/VSD.Right_femur.XX.XX.OT.101152.0.stl"
+      case 7 => "data/partials/VSD.Right_femur.XX.XX.OT.101153.0.stl"
+      case 8 => "data/partials/VSD.Right_femur.XX.XX.OT.101154.0.stl"
+      case 9 => "data/partials/VSD.Right_femur.XX.XX.OT.101155.0.stl"
+      case 10 => "data/partials/VSD.Right_femur.XX.XX.OT.101156.0.stl"
+    }
+
+
+    val target: TriangleMesh = MeshIO.readMesh(new File(targetName)).get
     ui.show(target, "partialShape")
 
     println("Loading and displaying statistical shape model...")
-    val model: StatisticalMeshModel = StatismoIO.readStatismoMeshModel(new File("data/data_shape_model.h5")).get
+    val model: StatisticalMeshModel = StatismoIO.readStatismoMeshModel(new File("data/augmented_shape_model.h5")).get
     ui.show(model, "model")
 
 
     val pointSamples = UniformMeshSampler3D(model.mean, 5000, 42).sample.map(s => s._1)
     val pointIds = pointSamples.map { s => model.mean.findClosestPoint(s).id }
 
-    //Femur 1
-    //val p = new Point3D(-40.558f, 26.1689f, -208.722f)
-    //val correctedPointIds = pointIds.filter{ id : PointId =>   (model.referenceMesh.point(id) - p).norm > 62}
 
-    //Femur 2
-    //val correctedPointIds = pointIds.filter{ id : PointId =>   model.referenceMesh.point(id).z <50.7622 }
+    val filteredPointIds = reconstructFemurNo match {
+      case 1 => pointIds.filter { id: PointId => (model.referenceMesh.point(id) - Point3D(-40.558f, 26.1689f, -208.722f)).norm > 62 }
+      case 2 => pointIds.filter { id: PointId => model.referenceMesh.point(id).z < 50.7622 }
+      case 3 => pointIds.filter { id: PointId => (91.0552 * model.referenceMesh.point(id).x + 31.6728 * model.referenceMesh.point(id).y + 10.516 * model.referenceMesh.point(id).z - 2294.54 < -100) }
+      case 4 => pointIds.filter { id: PointId => model.referenceMesh.point(id).z < -83.69 || model.referenceMesh.point(id).z > 93.922 }
+      case 5 => pointIds.filter { id: PointId => model.referenceMesh.point(id).z > -30.639 }
+      case 6 => pointIds.filter { id: PointId => model.referenceMesh.point(id).z < -136 || model.referenceMesh.point(id).z > 158 }
+      case 7 => pointIds.filter { id: PointId => model.referenceMesh.point(id).z > -84 || model.referenceMesh.point(id).z < -172 }
+      case 8 => pointIds.filter { id: PointId => model.referenceMesh.point(id).z > -134 }
+      case 9 => pointIds.filter { id: PointId => model.referenceMesh.point(id).x < 3.5 && model.referenceMesh.point(id).y < 33.7 && model.referenceMesh.point(id).z > -169.9 }
+      case 10 => pointIds.filter { id: PointId => model.referenceMesh.point(id).z < 180 }
+    }
 
-    //Femur 4
-    //val correctedPointIdsA = pointIds.filter { id: PointId => model.referenceMesh.point(id).z < -83.69}
-    //val correctedPointIdsB = pointIds.filter { id: PointId =>   model.referenceMesh.point(id).z > 93.922 }
-
-    //val correctedPointIds = correctedPointIdsA.union(correctedPointIdsB)
-
-    //Femur 5
-    //val correctedPointIds = pointIds.filter { id: PointId => model.referenceMesh.point(id).z > -30.639 }
-
-    //Femur 6
-    //from tries: best with data Model, 50 iterations
-    //val correctedPointIds = pointIds.filter { id: PointId =>  model.referenceMesh.point(id).z < -136 ||
-    //                                                          model.referenceMesh.point(id).z > 158 }
-
-    //Femur 7
-    //val correctedPointIds = pointIds.filter { id: PointId =>  model.referenceMesh.point(id).z > -84 ||
-    //                                                          model.referenceMesh.point(id).z < -172 }
-
-    //Femur 8
-    //tried with: 50 iterations, augmented model
-    //val correctedPointIds = pointIds.filter { id: PointId => model.referenceMesh.point(id).z > -134 }
-
-    //Femur 9
-    //tried with: 50 iterations, augmented model
-    //val correctedPointIds = pointIds.filter { id: PointId =>  model.referenceMesh.point(id).x < 3.5 &&
-     //                                                         model.referenceMesh.point(id).y < 33.7 &&
-     //                                                         model.referenceMesh.point(id).z > -169.9
-    //}
-    //femur 10
-    //val correctedPointIds = pointIds.filter { id: PointId => model.referenceMesh.point(id).z < 180 }
-
-    //femur 3
-    val correctedPointIds = pointIds.filter { id: PointId =>  91.0552 * model.referenceMesh.point(id).x +
-                                                              31.6728*model.referenceMesh.point(id).y +
-                                                              10.516*model.referenceMesh.point(id).z -
-                                                              2294.54  < -100 }
-    ui.show(correctedPointIds.map { id => model.mean.point(id) }, "points")
+    ui.show(filteredPointIds.map { id => model.mean.point(id) }, "points")
 
 
     def attributeCorrespondences(pts: Seq[Point[_3D]]): Seq[Point[_3D]] = {
@@ -97,8 +85,8 @@ object FitFemur {
     def recursion(currentPoints: Seq[Point[_3D]], nbIterations: Int): Seq[Point[_3D]] = {
 
       val candidates = attributeCorrespondences(currentPoints)
-      val fit = fitModel(correctedPointIds, candidates)
-      val newPoints = correctedPointIds.map(id => fit.point(id))
+      val fit = fitModel(filteredPointIds, candidates)
+      val newPoints = filteredPointIds.map(id => fit.point(id))
       if (nbIterations > 0) {
         //Thread.sleep(1000)
 
@@ -112,9 +100,9 @@ object FitFemur {
 
 
     println("Performing fitting recursion and calculating correspondences...")
-    val fittedModelPoints = recursion(correctedPointIds.map(id => model.mean.point(id)), numIterations)
+    val fittedModelPoints = recursion(filteredPointIds.map(id => model.mean.point(id)), numIterations)
     val targetPoints = attributeCorrespondences(fittedModelPoints)
-    val refPoints = correctedPointIds.map(id => model.mean.point(id))
+    val refPoints = filteredPointIds.map(id => model.mean.point(id))
 
     val domain = UnstructuredPointsDomain[_3D](refPoints.toIndexedSeq)
     val diff = refPoints.zip(targetPoints).map { case (mPt, pPt) => pPt - mPt }
